99d933217737bd13b8785ad3e40986f6938ef8ad
Ivan Shmakov <i.s.shmakov@gmail.com>
2019-02-21 01:24:48 +0700

    feat(stack): add realization of stack class
    Add classic stack realisation/ Also add exceptios.

--- src/interpreter/resources/exceptions.py
+++ src/interpreter/resources/exceptions.py
@@ -1,5 +1,11 @@
 class InvalidMemorySize(RuntimeError):
     pass
-    
+
 class InvalidStackSize(RuntimeError):
+    pass
+
+class MyStackOverflow(RuntimeError):
+    pass
+
+class EmptyStack(RuntimeError):
     pass
--- src/interpreter/resources/stack.py
+++ src/interpreter/resources/stack.py
@@ -1,2 +1,38 @@
+from .exceptions import InvalidStackSize, MyStackOverflow, EmptyStack
+
+from collections import deque
+from copy import deepcopy
+
+
 class Stack:
-    pass
+    _max_size = 1000
+
+    def __init__(self, stack_size: int = 10) -> None:
+        if stack_size < 1 or stack_size > self._max_size:
+            raise InvalidStackSize(
+                f"Stack size must be between 0 and {self._max_size}")
+        self._stack_size = stack_size
+        self._stack = deque(maxlen=self._stack_size)
+
+    @property
+    def stack_size(self) -> int:
+        return self._stack_size
+
+    @property
+    def stack(self) -> list:
+        return list(deepcopy(self._stack))
+
+    def push(self, value: any) -> None:
+        if len(self._stack) >= self._stack_size:
+            raise MyStackOverflow("Stack overflow")
+        self._stack.append(value)
+
+    def pop(self) -> any:
+        if len(self._stack < 1):
+            raise EmptyStack("Stack is empty")
+        return self._stack.pop()
+
+    def peek(self) -> any:
+        if len(self._stack < 1):
+            raise EmptyStack("Stack is empty")
+        return self._stack[-1]
