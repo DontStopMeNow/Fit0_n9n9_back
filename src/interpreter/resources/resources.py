487869b6d8fe6343161807cbbeedda72d35ccc8f
Ivan Shmakov <i.s.shmakov@gmail.com>
2019-02-21 01:49:12 +0700

    feat(resources): add resources class realization
    Resourcess class is container of resources
    such as Memory and Stack.

--- src/interpreter/resources/resources.py
+++ src/interpreter/resources/resources.py
@@ -1,2 +1,16 @@
+from stack import Stack
+from memory import Memory
+
+
 class Resources:
-    pass
+    def __init__(self, stack: Stack = Stack(100), memory: Memory = Memory(100)) -> None:
+        self._memory = memory
+        self._stack = stack
+
+    @property
+    def memory(self) -> Memory:
+        return self._memory
+
+    @property
+    def stack(self) -> Stack:
+        return self._stack
