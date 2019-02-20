b3dedb9084eb08bfa946e4a13daa49964daa9a11
Ivan Shmakov <i.s.shmakov@gmail.com>
2019-02-21 01:41:26 +0700

    refactor(stack): rename stack size variable
    Rename from _stack_size to _size. Also rename property from stack_size to size

--- src/interpreter/resources/stack.py
+++ src/interpreter/resources/stack.py
@@ -7,23 +7,23 @@ from copy import deepcopy
 class Stack:
     _max_size = 1000
 
-    def __init__(self, stack_size: int = 10) -> None:
-        if stack_size < 1 or stack_size > self._max_size:
+    def __init__(self, size: int = 10) -> None:
+        if size < 1 or size > self._max_size:
             raise InvalidStackSize(
                 f"Stack size must be between 0 and {self._max_size}")
-        self._stack_size = stack_size
-        self._stack = deque(maxlen=self._stack_size)
+        self._size = size
+        self._stack = deque(maxlen=size)
 
     @property
-    def stack_size(self) -> int:
-        return self._stack_size
+    def size(self) -> int:
+        return self._size
 
     @property
     def stack(self) -> list:
         return list(deepcopy(self._stack))
 
     def push(self, value: any) -> None:
-        if len(self._stack) >= self._stack_size:
+        if len(self._stack) >= self._size:
             raise MyStackOverflow("Stack overflow")
         self._stack.append(value)
 
