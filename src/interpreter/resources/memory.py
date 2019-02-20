af99b7481bc29cd7686c7a021819b2ebaaa79779
Ivan Shmakov <i.s.shmakov@gmail.com>
2019-02-21 01:18:38 +0700

    refactor(memory): add returning types hints
    Add type hints for all methods

--- src/interpreter/resources/memory.py
+++ src/interpreter/resources/memory.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 class Memory:
     _max_memory_size = 1000
 
-    def __init__(self, memory_size: int = 10):
+    def __init__(self, memory_size: int = 10) -> None:
         if memory_size < 1 or memory_size > self._max_memory_size:
             raise InvalidMemorySize(
                 f"Memory size must be between 0 and {self._max_memory_size}")
@@ -15,33 +15,33 @@ class Memory:
         self._position = 0
 
     @property
-    def memory_size(self):
+    def memory_size(self) -> int:
         return self.memory_size
 
     @property
-    def position(self):
+    def position(self) -> int:
         return self._position
 
     @property
-    def memory(self):
+    def memory(self) -> list:
         return deepcopy(self._memory)
 
     @property
-    def value(self):
+    def value(self) -> int:
         return self._memory[self._position]
 
-    def inc_position(self):
+    def inc_position(self) -> None:
         self._position += 1
         if self.position > self._memory_size:
             self._position %= self._memory_size
 
-    def dec_position(self):
+    def dec_position(self) -> None:
         self._position -= 1
         if self._position < 0:
             self._position %= self._memory_size
-    
-    def inc_value(self):
+
+    def inc_value(self) -> None:
         self._memory[self._position] += 1
 
-    def dec_value(self):
+    def dec_value(self) -> None:
         self._memory[self._position] -= 1
